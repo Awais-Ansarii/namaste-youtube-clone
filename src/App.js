b1df18be75938ff34ce42d848d8291a5370e33a8
Awais Ansari <127539725+Awais-Ansarii@users.noreply.github.com>
2023-05-12 14:10:00 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+import Body from './components/Body';
+import Header from './components/Header';
+import { createBrowserRouter , RouterProvider } from 'react-router-dom';
+import WatchVideo from './components/WatchVideo';
+import MainContainer from './components/MainContainer';
+import ErrorPage from './components/ErrorPage';
+import SearchResults from './components/SearchResults';
+import ChannelVideoList from './components/ChannelVideoList';
+import ShowAllSubscriptions from './components/ShowAllSubscriptions';
+import LiveVideosList from './components/LiveVideosList';
+function App() {
+  /*
+    APP LAYOUT
+
+    Header
+    Body
+      SideBar
+        Menu Items
+      Main Container
+        Filter Buttons List
+        Video Container
+          Video Card
+
+  */
+
+  const appRouter = createBrowserRouter([
+    {
+      path: '/',
+      element: <><Header /><Body /></>,
+      errorElement : <ErrorPage />,
+      children:[
+        {
+          path: '/',
+          element: < MainContainer />
+        },
+        {
+          path: 'watch',
+          element : < WatchVideo />
+        },
+        {
+          path : 'results',
+          element : <SearchResults />
+        },
+        {
+          path: 'channel',
+          element : <ChannelVideoList />
+        },
+        {
+          path: 'subscriptions',
+          element: <ShowAllSubscriptions />
+        },
+        {
+          path: 'live',
+          element: <LiveVideosList />
+        }
+      ]
+    }
+  ])
+
+  return (
+    <div>
+        <RouterProvider router={appRouter} />
+    </div>
+  );
+}
+
+export default App;
